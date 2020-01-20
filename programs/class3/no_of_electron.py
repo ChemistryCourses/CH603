c630f60edf16433ade8758cea6eb720b3679ad5e
achintya <achintya007@iitb.ac.in>
2020-01-20 17:39:06 +0530

    third class

---
+++ programs/class3/no_of_electron.py
@@ -0,0 +1,17 @@
+#the code is to find the no of electron in the atom
+print('Write the name of the atom')
+atom=input()
+symbol = [
+            'H','He',
+            'Li','Be','B','C','N','O','F','Ne',
+            'Na','Mg','Al','Si','P','S','Cl','Ar',
+            'K', 'Ca', 'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe',
+            'Co', 'Ni', 'Cu', 'Zn',
+            'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr',
+            'Rb', 'Sr', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru',
+            'Rh', 'Pd', 'Ag', 'Cd',
+            'In', 'Sn', 'Sb', 'Te', 'I', 'Xe',
+            'Cs', 'Ba', 'La', 'Ce', 'Pr', 'Nd', 'Pm', 'Sm',  'Eu',
+            'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu',
+            'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg',
+            'Tl','Pb','Bi','Po','At','Rn']
