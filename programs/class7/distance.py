b8e578df9b8313122c4b3e3e15d84d6b473a3f34
achintya <achintya007@iitb.ac.in>
2020-02-04 15:23:27 +0530

    class7

---
+++ programs/class7/distance.py
@@ -0,0 +1,8 @@
+def find_distance(a,b):
+
+O=[0.000000000000,-0.143225816552,0.000000000000]
+H=[1.638036840407,1.136548822547,-0.000000000000]
+dist=find_distance(O,H)
+print('distance_between O and H '+str(dist))
+
+ 
